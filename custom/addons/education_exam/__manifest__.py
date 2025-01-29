c01b79a08a073ab132cfc07739b1cbb0f7fa15f1
unknown <ashin.krishnan@zaeemsolutions.com>
2025-01-29 14:41:55 +0530

    final_version

---
+++ custom/addons/education_exam/__manifest__.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#    A part of Educational ERP Project <https://www.educationalerp.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#    Copyright (C) 2020-TODAY Cybrosys Technologies (<https://www.cybrosys.com>)
+#    Author: Hajaj Roshan (hajaj@cybrosys.in)
+#
+#    This program is free software: you can modify
+#    it under the terms of the GNU Affero General Public License (AGPL) as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Educational Exam Management',
+    'version': '15.0.1.0.0',
+    'summary': """Sneak the Examination management in Educational ERP""",
+    'description': 'An easy way to handle the examinations in an educational '
+                   'system with better reports and exam valuation and exam result '
+                   'facilities',
+    'category': 'Industries',
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.educationalerp.com",
+    'depends': ['base_setup','web'],
+    'data': [
+        'security/education_security.xml',
+        'security/ir.model.access.csv',
+        #'views/education_academic_year.xml',
+        'Data/data.xml',
+        'views/examination.xml',
+        'views/q_paper_template.xml',
+        'views/q_paper_template_view.xml',
+        'views/question_answer_template.xml',
+
+        #'views/odoo_customization.xml'
+
+        # 'views/exam_valuation.xml',
+        # 'views/exam_results.xml',
+        # 'views/student_view.xml',
+        # 'views/template.xml',
+    ],
+    # 'qweb': ['static/src/xml/base.xml'],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
